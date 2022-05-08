41532bdf6b8b1f52f92fd088cc83cd0eb629358d
takewiki <hulilei@takewiki.com.cn>
2022-05-09 00:33:51 +0800

    v0.4

---
+++ R/roleAdd.R
@@ -0,0 +1,173 @@
+#' 检测角色是否存在
+#'
+#' @param conn 连接
+#' @param app_id 程序ID
+#' @param role_name 角色名
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' roleAdd_isNew()
+roleAdd_isNew <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',role_name='standard') {
+sql <- paste0(" select  FRoleName  from t_md_role
+  where FappId ='",app_id,"' and  FRoleName='",role_name,"'")
+data = tsda::sql_select(conn,sql)
+ncount =nrow(data)
+if(ncount>0){
+  res = FALSE
+}else{
+  res = TRUE
+}
+return(res)
+
+
+}
+
+
+#' 检测角色是否存在
+#'
+#' @param conn 连接
+#' @param app_id 程序ID
+#' @param role_name 角色名
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' roleAdd_isNew()
+roleAdd_query <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',role_name='standard') {
+  sql <- paste0(" select   FRoleName as 角色名称,FNote as 角色备注
+ from t_md_role
+  where FappId ='",app_id,"' and  FRoleName='",role_name,"'")
+  data = tsda::sql_select(conn,sql)
+
+  return(data)
+
+
+}
+
+
+#' 检测角色是否存在
+#'
+#' @param conn 连接
+#' @param app_id 程序ID
+#' @param role_name 角色名
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' roleAdd_isNew()
+roleAdd_query <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',role_name='standard') {
+  sql <- paste0(" select   FRoleName as 角色名称,FNote as 角色备注
+ from t_md_role
+  where FappId ='",app_id,"' and  FRoleName='",role_name,"'")
+  data = tsda::sql_select(conn,sql)
+
+  return(data)
+
+
+}
+
+#' 检测角色最大值
+#'
+#' @param conn 连接
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' roleAdd_isNew()
+roleAdd_MaxId <- function(conn = tsda::conn_rds('rdbe')) {
+
+  data = tsda::db_maxId(conn = conn,FTableName = 't_md_role')
+
+  return(data)
+
+
+}
+
+
+#' 检测角色是否存在
+#'
+#' @param conn 连接
+#' @param app_id 程序ID
+#' @param note 备注
+#' @param role_name 角色名
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#'
+#' roleAdd_save()
+roleAdd_save <- function(conn = tsda::conn_rds('rdbe'),
+                         app_id ='cpdms',role_name='demo',note='') {
+
+  flag = roleAdd_isNew(conn = conn,app_id = app_id ,role_name = role_name)
+  if(flag){
+    FInterId = roleAdd_MaxId(conn = conn) + 1
+    sql <- paste0("  insert into t_md_role
+    values(1,'",app_id,"','",role_name,"','",note,"',0)")
+    tsda::sql_update(conn,sql)
+    res = TRUE
+
+  }else{
+    res = FALSE
+  }
+  return(res)
+
+}
+
+#' 角色新增的逻辑处理
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param app_id 程序
+#' @param session 会话
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' roleAddServer()
+roleAddServer <- function(input,output,session,app_id){
+  var_roleName = tsui::var_text('roleAdd_roleName_txt')
+  var_note = tsui::var_text('roleAdd_note_txt')
+  shiny::observeEvent(input$roleAdd_upload_btn,{
+    role_name = var_roleName()
+    note= var_note()
+    print(role_name)
+    if(role_name ==''){
+      tsui::pop_notice("请输入角色名!")
+    }else{
+      if(note ==''){
+        tsui::pop_notice("请输入角色备注!")
+
+      }else{
+
+        flag = roleAdd_save(app_id = app_id,role_name = role_name,note = note)
+        data = roleAdd_query(app_id = app_id,role_name = role_name)
+        tsui::run_dataTable2(id = 'roleAdd_query_dataview',data = data)
+        if(flag){
+          tsui::pop_notice("角色新增成功!")
+        }else{
+          tsui::pop_notice("角色已经存在!")
+        }
+
+
+
+      }
+
+    }
+
+
+
+  })
+
+
+
+
+
+}
--- R/userQuery.R
+++ R/userQuery.R
@@ -1,4 +1,4 @@
-#'用户查询
+#'用户禁用
 #'
 #' @param conn 连接
 #' @param app_id 程序ID
@@ -9,25 +9,198 @@
 #'
 #' @examples
 #' userQuery_query()
-userQuery_query <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',user_name='') {
+userQuery_disable <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',user_name='') {
+  sql <-paste0("update a set a.Fdeleted =1 from t_md_userRight a
+  where  FappId ='",app_id,"' and  Fuser='",user_name,"'")
+  data = tsda::sql_update(conn,sql)
+
+  return(data)
+
+
+}
+#'用户反禁用
+#'
+#' @param conn 连接
+#' @param app_id 程序ID
+#' @param user_name 用户名
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' userQuery_query()
+userQuery_enable <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',user_name='') {
+  sql <-paste0("update a set a.Fdeleted =0 from t_md_userRight a
+  where  FappId ='",app_id,"' and  Fuser='",user_name,"'")
+  data = tsda::sql_update(conn,sql)
+
+  return(data)
+
+
+}
+
+
+#'用户查询
+#'
+#' @param conn 连接
+#' @param app_id 程序ID
+#' @param user_name 用户名
+#' @param status 禁用状态
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' userQuery_query()
+userQuery_query <- function(conn = tsda::conn_rds('rdbe'),app_id ='cpdms',user_name='',status=-1) {
   if(user_name == ''){
-    sql <- paste0("SELECT
-Fuser as  用户名
-,fname as 真实姓名
-,Fpermissions as 角色
-  FROM t_md_userRight")
+    if(status == -1){
+      sql <- paste0("SELECT
+a.Fuser as  用户名
+,a.fname as 真实姓名
+,a.Fpermissions as 角色
+,b.fname as  是否禁用
+  FROM t_md_userRight  a
+  inner join t_md_logical b
+  on a.Fdeleted = b.fid  where a.FappId ='",app_id,"'")
+    }else{
+      sql <- paste0("SELECT
+a.Fuser as  用户名
+,a.fname as 真实姓名
+,a.Fpermissions as 角色
+,b.fname as  是否禁用
+  FROM t_md_userRight  a
+  inner join t_md_logical b
+  on a.Fdeleted = b.fid  where a.FappId ='",app_id,"'   and  b.fid =  ",status,"")
+    }
+
   }else{
-    sql <- paste0("SELECT
-Fuser as  用户名
-,fname as 真实姓名
-,Fpermissions as 角色
-  FROM t_md_userRight
-  where FappId ='",app_id,"' and  Fuser like'%",user_name,"%'")
+    if(status == -1){
+      sql <- paste0("SELECT
+a.Fuser as  用户名
+,a.fname as 真实姓名
+,a.Fpermissions as 角色
+,b.fname as  是否禁用
+  FROM t_md_userRight  a
+  inner join t_md_logical b
+  on a.Fdeleted = b.fid
+  where a.FappId ='",app_id,"' and  a.Fuser like'%",user_name,"%'")
+    }else{
+      sql <- paste0("SELECT
+a.Fuser as  用户名
+,a.fname as 真实姓名
+,a.Fpermissions as 角色
+,b.fname as  是否禁用
+  FROM t_md_userRight  a
+  inner join t_md_logical b
+  on a.Fdeleted = b.fid
+  where a.FappId ='",app_id,"' and  a.Fuser like'%",user_name,"%'    and  b.fid =  ",status,"")
+    }
+
   }
 
   data = tsda::sql_select(conn,sql)
 
   return(data)
+
+
+}
+
+
+#' 用户新增的逻辑处理
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param app_id 程序
+#' @param session 会话
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' userQueryServer_query()
+userQueryServer_query <- function(input,output,session,app_id){
+  var_userName = tsui::var_text('userQuery_userName_txt')
+  var_userQuery_status_lc1 <- tsui::var_ListChoose1('userQuery_status_lc1')
+
+
+  shiny::observeEvent(input$userQuery_query_btn,{
+    user_name = var_userName()
+    status = var_userQuery_status_lc1()
+    print(status)
+    data = userQuery_query(app_id = app_id,user_name = user_name,status = status)
+    tsui::run_dataTable2(id = 'userQuery_query_dataview',data = data)
+
+  })
+
+
+
+}
+
+
+
+
+
+
+#' 用户新增的逻辑处理
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param app_id 程序
+#' @param session 会话
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' userQueryServer_query()
+userQueryServer_disable <- function(input,output,session,app_id){
+
+  var_userName = tsui::var_text('userQuery_userName_txt')
+  shiny::observeEvent(input$userQuery_disable_btn,{
+    user_name = var_userName()
+    flag = userAdd_isNew(app_id=app_id,user_name=user_name)
+    if(flag){
+       tsui::pop_notice('请重新选择用户')
+    }else{
+      userQuery_disable(app_id = app_id,user_name = user_name)
+      tsui::pop_notice(paste0("用户:",user_name,"禁用成功!"))
+    }
+
+
+  })
+
+
+}
+
+
+#' 用户新增的逻辑处理
+#'
+#' @param input 输入
+#' @param output 输出
+#' @param app_id 程序
+#' @param session 会话
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' userQueryServer_query()
+userQueryServer_enable <- function(input,output,session,app_id){
+
+  var_userName = tsui::var_text('userQuery_userName_txt')
+  shiny::observeEvent(input$userQuery_enable_btn,{
+    user_name = var_userName()
+    flag = userAdd_isNew(app_id=app_id,user_name=user_name)
+    if(flag){
+      tsui::pop_notice('请重新选择用户')
+    }else{
+      userQuery_enable(app_id = app_id,user_name = user_name)
+      tsui::pop_notice(paste0("用户:",user_name,"反禁用成功!"))
+    }
+
+
+  })
 
 
 }
@@ -45,14 +218,9 @@ Fuser as  用户名
 #' @examples
 #' userQueryServer()
 userQueryServer <- function(input,output,session,app_id){
-  var_userName = tsui::var_text('userQuery_userName_txt')
-
-  shiny::observeEvent(input$userQuery_query_btn,{
-    user_name = var_userName()
-    data = userQuery_query(app_id = app_id,user_name = user_name)
-    tsui::run_dataTable2(id = 'userQuery_query_dataview',data = data)
-
-  })
+ userQueryServer_query(input,output,session,app_id)
+  userQueryServer_disable(input,output,session,app_id)
+  userQueryServer_enable(input,output,session,app_id)
 
 
 
